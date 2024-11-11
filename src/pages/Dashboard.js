307aa9158f7156a730385efab45fe458c796824f
John Albert <JAlbert@launchcg.com>
2024-11-11 15:19:14 -0800

    update app

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -15,7 +15,7 @@ const Dashboard = () => {
         </h5>
         <iframe
           title="PowerBI Report"
-          width="75%"
+          width="100%"
           height="500px"
           src="https://app.powerbi.com/reportEmbed?reportId=a436db24-a94f-4b18-a71e-fd5888b92541&autoAuth=true&ctid=fbe4560f-c07e-4c03-9943-e49aa26f8c94"
           frameBorder="0"
